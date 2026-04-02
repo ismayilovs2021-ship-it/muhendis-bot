bd8bd52baca030414b7e4d2085f4d016e5b771da
ismayilovs2021-ship-it <ismayilovs2021@gmail.com>
2026-04-03 00:50:28 +0400

    Change sleep duration in main.py
    Reduce sleep time from 3.5 hours to 100 seconds.

--- main.py
+++ main.py
@@ -129,7 +129,7 @@ def xeber_paylas():
                 yaddas.append(link)
                 if len(yaddas) > 50: yaddas.pop(0)
                 
-                time.sleep(10800) # 3.5 saat gözləmə
+                time.sleep(100) # 3.5 saat gözləmə
             else:
                 time.sleep(30)
                 
